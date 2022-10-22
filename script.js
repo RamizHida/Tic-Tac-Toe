c5424d259eeb1c9d8a2eabc2f7badef303fbf7d3
Ramiz Hida <ramizhida00@gmail.com>
2022-10-22 19:53:25 +0200

    set up skeleton for project

---
+++ script.js
@@ -0,0 +1,16 @@
+const gameBoard = (function () {
+  let field = document.querySelector(".container");
+  let board = ["x", "x", "x", "o", "o", "o", "x", "x", "x"];
+  displayPlayer = function () {
+    //loop through container
+    //in each iteration display one element from board array to one child of the container, until the last element / last element in the board array
+  };
+  return { displayPlayer };
+})();
+
+const players = function () {
+  let playerOne = [];
+  let playerTwo = [];
+};
+
+displayPlayer();
