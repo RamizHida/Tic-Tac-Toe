8a90d7857f09b43d48339c4061695dca58975e24
Ramiz Hida <ramizhida00@gmail.com>
2022-10-23 12:22:05 +0200

    Set dummy input on game board and added play button

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
+// Game start
+let play = document.querySelector(".play--btn");
+let trackPlayer = document.querySelector(".players");
+play.addEventListener("click", startgame);
+
+function startgame() {
+  play.style.display = "none";
+  trackPlayer.style.display = "flex";
+}
+
+// Create gameboard
 const gameBoard = (function () {
   let field = document.querySelector(".container");
+  let children = field.children;
   let board = ["x", "x", "x", "o", "o", "o", "x", "x", "x"];
-  displayPlayer = function () {
-    //loop through container
-    //in each iteration display one element from board array to one child of the container, until the last element / last element in the board array
-  };
-  return { displayPlayer };
+  display: function display() {
+    for (let i = 0; i < children.length; i++) {
+      let displayChoice = document.createElement("span");
+      displayChoice.textContent = board[i];
+      children[i].appendChild(displayChoice);
+    }
+  }
+  return { display };
 })();
 
-const players = function () {
-  let playerOne = [];
-  let playerTwo = [];
+gameBoard.display();
+
+// Create players
+const Players = function (name, weapon) {
+  const playerInto = () => {
+    console.log(
+      `Welcome ${name}, your weapon of choice is ${weapon.toUpperCase()}`
+    );
+  };
+  return { name, weapon, playerInto };
 };
 
-displayPlayer();
+const player1 = new Players("Ramiz", "O");
+const player2 = new Players("Riad", "x");
+
+player1.playerInto();
+player2.playerInto();
+
+//
